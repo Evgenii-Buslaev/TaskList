b45d8cae2b1d8fcfc1049f769ed9408b37f7534e
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-08-22 17:27:28 +0500

    Added logic of addind and deleting tasks and their rendering

--- src/scripts/handlers/renderAddedTasks.js
+++ src/scripts/handlers/renderAddedTasks.js
@@ -1,11 +1,18 @@
 import Task from "../Classes/Task.js";
 import { taskContainer } from "../constants.js";
 
+import { addApiTask } from "../handlers/add.js";
+import { deleteApiTask } from "../handlers/delete.js";
+
 export function renderAddedTask() {
   let task = new Task(
+    null,
     document.getElementById("task-input").value,
-    taskContainer
+    taskContainer,
+    addApiTask,
+    deleteApiTask
   );
   task.generateTask();
   task.renderTask();
+  task.setHandlers();
 }
--- src/scripts/handlers/renderServerChanges.js
+++ src/scripts/handlers/renderServerChanges.js
@@ -1,5 +1,7 @@
 import Task from "../Classes/Task.js";
 import { taskContainer } from "../constants.js";
+import { addApiTask } from "../handlers/add.js";
+import { deleteApiTask } from "../handlers/delete.js";
 
 // function for rendering server data after loading the page
 
@@ -7,9 +9,13 @@ async function renderServerChanges(object) {
   let checking = setInterval(() => {
     if (object.data_recieved) {
       object.data_array.forEach((element) => {
-        let task = new Task(element.task, taskContainer);
+        let task = new Task(element.id, element.task, taskContainer, {
+          addApiTask,
+          deleteApiTask,
+        });
         task.generateTask();
         task.renderTask();
+        task.setHandlers();
       });
       clearInterval(checking);
     }
