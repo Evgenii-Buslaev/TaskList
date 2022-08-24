09149032a4f165adc80fb5991bac7179d6a2addb
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-08-24 15:20:26 +0500

    Added request for saving task text changes

--- src/scripts/handlers/edit.js
+++ src/scripts/handlers/edit.js
@@ -1,3 +1,22 @@
-export function editTask(id) {
+export async function editTask(id) {
   console.log(id);
+
+  let task = { task: `${document.getElementById(id).children[0].textContent}` };
+
+  let data = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(task),
+  };
+
+  let patchRequest = fetch(
+    `https://animesonproject.herokuapp.com/api/v1/task/${id}/edit`,
+    data
+  );
+
+  let editedTask = await patchRequest;
+  let responseRequest = await editedTask.json();
+  console.log(responseRequest);
 }
